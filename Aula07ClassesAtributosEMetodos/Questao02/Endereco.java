b32897c6db44028b238770d104c13d0376d25190
Eduardo Braga <dudubraga2004@gmail.com>
2024-03-19 12:00:55 -0300

    Questão02

---
+++ Aula07ClassesAtributosEMetodos/Questao02/Endereco.java
@@ -0,0 +1,32 @@
+package Aula07ClassesAtributosEMetodos.Questao02;
+
+public class Endereco {
+    String rua;
+    String numero;
+    String complemento;
+    String bairro;
+    String cidade;
+    String uf;
+    String cep;
+
+    public Endereco(String rua, String numero, String complemento, 
+    String bairro, String cidade, String uf, String cep){
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+    } 
+    public void modificarEndereco(String rua, String numero, String complemento, 
+    String bairro, String cidade, String uf, String cep){
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+    } 
+}
--- Aula07ClassesAtributosEMetodos/Questao02/Main.java
+++ Aula07ClassesAtributosEMetodos/Questao02/Main.java
@@ -1,5 +1,25 @@
 package Aula07ClassesAtributosEMetodos.Questao02;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Endereco end = new Endereco("do Principe", "526", 
+        "Bloco B", "Boa Vista", "Recide", 
+        "PE", "50050-900");
+        
+        System.out.print("Rua " + end.rua + ", nº " + end.numero);
+        System.out.println("Complemento: " + end.complemento);
+        System.out.println("Bairro: " + end.bairro);
+        System.out.println("Cidade: " + end.cidade);
+        System.out.println("UF: " + end.uf);
+        System.out.println("CEP: " + end.cep);
+
+        Medico med = new Medico("40028922", "Yudi", 
+        "M", "Urologia", end);
+
+        System.out.println("Dr(a) " + med.nome);
+        System.out.println("Código: " + med.codigo);
+        System.out.println("Sexo: " + med.sexo);
+        System.out.println("Especialidade: " + med.especialidade);
+
+    }    
 }
